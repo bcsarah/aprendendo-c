e2642a8c8e9597a518f6f2839a4d60d0ac27fe47
Better Call Sarah <bcsarah@proton.me>
2026-09-05 18:33:48 -0300

    int agora é float

--- ccalc.c
+++ ccalc.c
@@ -4,53 +4,44 @@
 #define println(...) printf(__VA_ARGS__), putchar('\n')
 
 // Funções Matemáticas
-int somar(int x, int y) {
+float somar(float x, float y) {
   return x + y;
 };
 
-int diminuir(int x, int y) {
+float diminuir(float x, float y) {
   return x - y;
 };
 
-int multiplicar(int x, int y) {
+float multiplicar(float x, float y) {
   return x * y;
 };
 
-int dividir(int x, int y) {
+float dividir(float x, float y) {
   return x / y;
 };
 
-int aoquadrado(int x) {
+float aoquadrado(float x) {
   return x * x;
-};
-
-int resto(int x, int y) {
-  return x % y;
 };
 
 /* ===== MAIN ===== */
 int main(int argc, char *argv[]) {
 
   // checa a quantidade de argumentos
-  if (argc > 3 || argc == 1 || argc == 2) {
+  if (argc != 3) {
     println("ERROR: ccalc <x> <y>");
     return 1;
   }
 
-  // atoi converte string p/ int
-  int x = atoi(argv[1]);
-  int y = atoi(argv[2]);
+  // atof converte string p/ float
+  float x = atof(argv[1]);
+  float y = atof(argv[2]);
 
   // output
-  println("== calc results ==");
-
-  println("\n%d + %d = %d", x, y, somar(x, y));
-  println("%d - %d = %d", x, y, diminuir(x, y));
-  println("%d * %d = %d", x, y, multiplicar(x, y));
-  println("%d / %d = %d", x, y, dividir(x, y));
-
-  println("\n%d ^ %d = %d", x, x, aoquadrado(x));
-  println("%d ^ %d = %d", y, y, aoquadrado(y));
-  println("%d %% %d = %d", x, y, resto(x, y));
-  println("%d %% %d = %d", y, x, resto(y, x));
+  println("\n%.2f + %.2f = %.2f", x, y, somar(x, y));
+  println("%.2f - %.2f = %.2f", x, y, diminuir(x, y));
+  println("%.2f * %.2f = %.2f", x, y, multiplicar(x, y));
+  println("%.2f / %.2f = %.2f", x, y, dividir(x, y));
+  println("%.2f ^ 2 = %.2f", x, x, aoquadrado(x));
+  println("%.2f ^ 2 = %.2f", y, y, aoquadrado(y));
 }

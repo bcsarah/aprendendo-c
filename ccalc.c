1df8e4b207fa3bd9cdd4337b96b48ffbb7dc4100
Better Call Sarah <bcsarah@proton.me>
2026-09-06 13:59:50 -0300

    ccalc agora usa operadores

--- ccalc.c
+++ ccalc.c
@@ -17,30 +17,47 @@ float multiplicar(float x, float y) {
 };
 
 float dividir(float x, float y) {
+  if (x == 0)
+    printf("ERROR: 0 division error");
+    return 1;
   return x / y;
 };
 
-float aoquadrado(float x) {
-  return x * x;
+float potenciar(float x, float y) {
+  if (y == 0)
+    return 1;
+  
+  float sum = x;
+
+  for (int i = 0; i < y - 1; i++)
+    sum = sum * x;
+  return sum;
 };
 
 /* ===== MAIN ===== */
 int main(int argc, char *argv[]) {
   // checa a quantidade de argumentos
-  if (argc != 4) { // 0 = sem argumentos
+  if (argc != 4) { // 1 = sem argumentos, 4 = 3 argumentos
     println("ERROR: ccalc <x> <op> <y>");
     return 1;
   }
 
   // atof converte string p/ float
   float x = atof(argv[1]);
-  float y = atof(argv[2]);
+  char op = *argv[2];
+  float y = atof(argv[3]);
 
   // output
-  println("\n%.2f + %.2f = %.2f", x, y, somar(x, y));
-  println("%.2f - %.2f = %.2f", x, y, diminuir(x, y));
-  println("%.2f * %.2f = %.2f", x, y, multiplicar(x, y));
-  println("%.2f / %.2f = %.2f", x, y, dividir(x, y));
-  println("%.2f ^ 2 = %.2f", x, x, aoquadrado(x));
-  println("%.2f ^ 2 = %.2f", y, y, aoquadrado(y));
+  if (op == '+')
+    println("%.2f + %.0f = %.2f", x, y, somar(x, y));
+  else if (op == '-')
+    println("%.2f - %.2f = %.2f", x, y, diminuir(x, y));
+  else if (op == '*')
+    println("%.2f * %.2f = %.2f", x, y, multiplicar(x, y));
+  else if (op == '/')
+    println("%.2f / %.2f = %.2f", x, y, dividir(x, y));
+  else if (op == '^')
+    println("%.2f ^ %.2f = %.2f", x, y, potenciar(x, y));
+  else
+    println("ERROR: operation not in operations (+, -, *, /, ^)");
 }
